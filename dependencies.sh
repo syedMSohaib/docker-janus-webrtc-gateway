bebfad164ae6b51b444ee7bdb953283d167710bf
SyedMSohaib <syedmsohaib68@gmail.com>
2020-01-22 15:48:48 +0000

    fix dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,5 @@ apt-get install -y libmicrohttpd-dev libjansson-dev libnice-dev \
     libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
     libopus-dev libogg-dev libini-config-dev libcollection-dev \
     libconfig-dev pkg-config gengetopt libtool automake init-system-helpers \
-	libc6 libcurl3 libjansson4 libmicrohttpd12 libnice10 libogg0 libopus0 \
-	librabbitmq4 libsrtp2-1 libssl1.0.0 libwebsockets8
+	libc6 libcurl3 libjansson4 libnice10 libogg0 libopus0 \
+	librabbitmq4 libssl1.0.0
